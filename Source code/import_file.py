5323ce4c49a7185c08a3f53feacc2c10cbd4dafa
HMarsafy <36539806+HMarsafy@users.noreply.github.com>
2020-04-30 21:35:31 +0200

    Update import_file.py

--- Source code/import_file.py
+++ Source code/import_file.py
@@ -1,11 +1,11 @@
-from scipy.spatial import distance as dist
-from imutils.video import VideoStream
-from imutils import face_utils
-from threading import Thread
+import cv2
 import numpy as np
 import playsound
 import argparse
 import imutils
 import time
 import dlib
-import cv2
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
